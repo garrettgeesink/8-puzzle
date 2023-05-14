9e9aa44a239712e2ee588cc9438f2dfcaac92e23
garrettgeesink <chatmansave@gmail.com>
2023-05-14 16:24:01 -0700

    updated gitignore and added initial main.cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Includes the entire std library for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+
+    int total = 8;
+
+    cout << total;
+}
